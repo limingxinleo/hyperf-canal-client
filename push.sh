2ce984961f00ca8682d336debfe8c1a65c916a7b
李铭昕 <715557344@qq.com>
2021-08-31 11:38:22 +0800

    Update

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@
 
 docker login docker.pkg.github.com -u limingxinleo -p $GITHUB_TOKEN
 
-docker tag biz-skeleton:latest docker.pkg.github.com/limingxinleo/hyperf-canal-client/canal-client:$CLIENT_VERSION
-docker push docker.pkg.github.com/limingxinleo/hyperf-canal-client/canal-client:$CLIENT_VERSION
-docker tag biz-skeleton:latest docker.pkg.github.com/limingxinleo/hyperf-canal-client/canal-client:latest
-docker push docker.pkg.github.com/limingxinleo/hyperf-canal-client/canal-client:latest
+docker tag biz-skeleton:latest docker.pkg.github.com/limingxinleo/hyperf-canal-client/hyperf-canal-client:$CLIENT_VERSION
+docker push docker.pkg.github.com/limingxinleo/hyperf-canal-client/hyperf-canal-client:$CLIENT_VERSION
+docker tag biz-skeleton:latest docker.pkg.github.com/limingxinleo/hyperf-canal-client/hyperf-canal-client:latest
+docker push docker.pkg.github.com/limingxinleo/hyperf-canal-client/hyperf-canal-client:latest
