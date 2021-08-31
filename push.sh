86664a10d45bb8770427ba2d8777e62605e77156
李铭昕 <715557344@qq.com>
2021-08-31 11:56:00 +0800

    修改基础镜像地址

--- push.sh
+++ push.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-docker login docker.pkg.github.com -u limingxinleo -p $GITHUB_TOKEN
+echo $GITHUB_TOKEN | docker login ghcr.io -u limingxinleo --password-stdin
 
-docker tag biz-skeleton:latest docker.pkg.github.com/limingxinleo/hyperf-canal-client/hyperf-canal-client:${GITHUB_REF#refs/*/}
-docker push docker.pkg.github.com/limingxinleo/hyperf-canal-client/hyperf-canal-client:${GITHUB_REF#refs/*/}
-docker tag biz-skeleton:latest docker.pkg.github.com/limingxinleo/hyperf-canal-client/hyperf-canal-client:latest
-docker push docker.pkg.github.com/limingxinleo/hyperf-canal-client/hyperf-canal-client:latest
+docker tag biz-skeleton:latest ghcr.io/limingxinleo/hyperf-canal-client/hyperf-canal-client:${GITHUB_REF#refs/*/}
+docker push ghcr.io/limingxinleo/hyperf-canal-client/hyperf-canal-client:${GITHUB_REF#refs/*/}
+docker tag biz-skeleton:latest ghcr.io/limingxinleo/hyperf-canal-client/hyperf-canal-client:latest
+docker push ghcr.io/limingxinleo/hyperf-canal-client/hyperf-canal-client:latest
